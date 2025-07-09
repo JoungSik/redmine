ba7ae49bfed4b4ce8cbd28d5b7ea876b396c7ff5
Joungsik <tjstlr2010@gmail.com>
2025-07-10 08:33:28 +0900

    Done #17 PWA 지원

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,12 @@
 import "controllers"
+
+if ('serviceWorker' in navigator) {
+  // Register the service worker
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function (error) {
+      console.log('Service Worker registration failed:', error);
+    });
+}
